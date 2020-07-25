dd455bcf0615572535ec1a1c784f24f7c03908bb
SergiiPodlishchuk <podluy23@gmail.com>
2020-07-26 00:11:54 +0300

    Updates

---
+++ precache-manifest.ff84a2ec0afc515ee059e16dc98f14a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eec748f1eb63dbf30fbf7a076438f4b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9b2bf4c9921ced580578",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee012f9d7bf9009fcf16",
+    "url": "/goit-react-hw-02-feedback/static/js/main.4960f0a3.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
